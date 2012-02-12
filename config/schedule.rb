1abf37c3185157c835bcf8eea35024388eee5c4e
Jordan Byron <jordan.byron@gmail.com>
2012-02-12 12:58:47 -0500

    Set output of cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "/var/rapp/mail_whale/current/log/cron.log"
 
-job_type :command, "cd :path && bundle exec :task"
+job_type :command, "cd :path && bundle exec :task :output"
 
 every 1.minute do
   command "ruby mail_whale.rb"
